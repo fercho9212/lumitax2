30faf3026013321612b7cf372b698c2d310d8153
ferney <ferney9212@gmail.com>
2017-07-05 23:50:47 -0500

    vehicles

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Vehicle extends Model
 {
     //
+    protected $table="vehicles";
+    public $timestamps = true;
+    protected $primaryKey = 'id';
+    protected $fillable = [
+       'placa','veh_model','veh_motor','veh_serie',
+       'veh_vin','veh_color','veh_line','state_id',
+       'typevehicles_id','typelines_id','brands_id','leveles_id',
+    ];
+
 }
